c3ad73265fd4514dbaa13688b9134f2e068f6380
wtitze <w.titze@gmail.com>
2021-05-08 08:51:03 +0000

    first commit

--- main.py
+++ main.py
@@ -2,3 +2,5 @@ import pandas as pd
 
 df = pd.read_csv("https://raw.githubusercontent.com/italia/covid19-opendata-vaccini/master/dati/vaccini-summary-latest.csv", sep=',')
 print(df.head())
+dfNord = df[df['codice_regione_ISTAT']<=8]
+print(dfNord)
